8b2fe2fa6e10ceabb116b4005f7566533390bfb8
Lucas Choinski <lucas.choinski3030@gmail.com>
2025-04-21 01:28:49 -0300

    feat: Add nova aplicacao, api para consultar imc

---
+++ app.py
@@ -0,0 +1,42 @@
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+tarefas = []
+
+def calc(peso: float, altura: float):
+    imc = peso / (altura * altura)
+    return round(imc, 2)
+
+@app.route('/')
+def home():
+    return 'Bem-vindo à API de tarefas!'
+
+@app.route('/tarefas', methods=['GET'])
+def listar_tarefas():
+    return jsonify(tarefas)
+
+@app.route('/tarefas', methods=['POST'])
+def criar_tarefa():
+    descricao = request.json.get('descricao')
+    nova_tarefa = {'id': len(tarefas) + 1, 'descricao': descricao}
+    tarefas.append(nova_tarefa)
+    return jsonify(nova_tarefa), 201
+
+@app.route('/imc', methods=['POST'])
+def calcular_imc():
+    data = request.json
+    peso = data.get('peso')
+    altura = data.get('altura')
+    
+    if not peso or not altura:
+        return jsonify({'erro': 'Peso e altura são obrigatórios.'}), 400
+    
+    try:
+        imc = calc(float(peso), float(altura))
+        return jsonify({'imc': imc})
+    except Exception as e:
+        return jsonify({'erro': str(e)}), 400
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ test.py
@@ -0,0 +1,17 @@
+import requests
+
+url = "http://127.0.0.1:5000/imc"
+
+body = {
+    "peso": "50",
+    "altura": "1.70"
+}
+
+# Envia a requisição POST com o JSON
+resposta = requests.post(url, json=body)
+
+# Verifica se a requisição foi bem-sucedida
+if resposta.status_code == 200:
+    print('IMC calculado:', resposta.json()['imc'])
+else:
+    print('Erro:', resposta.json())
