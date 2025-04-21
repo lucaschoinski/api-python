90b92f6a4cd6997adc00bc1530df725340bd2892
Lucas Choinski <lucas.choinski3030@gmail.com>
2025-04-21 02:20:04 -0300

    feat: adicionando codigo para testar novo endpoit

--- test.py
+++ test.py
@@ -1,17 +1,33 @@
 import requests
 
-url = "http://127.0.0.1:5000/imc"
+url = "http://127.0.0.1:5000"
 
-body = {
+body_imc = {
     "peso": "50",
     "altura": "1.70"
 }
 
+body_tmb = {
+    "perfil": "H",
+    "peso": "50",
+    "altura": "1.60",
+    "idade": "45"
+}
+
 # Envia a requisição POST com o JSON
-resposta = requests.post(url, json=body)
+resposta_tmb = requests.post(url + '/tmb', json=body_tmb)
+# Envia a requisição POST com o JSON
+resposta_imc = requests.post(url + '/imc', json=body_imc)
 
 # Verifica se a requisição foi bem-sucedida
-if resposta.status_code == 200:
-    print('IMC calculado:', resposta.json()['imc'])
+if resposta_imc.status_code == 200:
+    print('IMC calculado:', resposta_imc.json()['imc'])
 else:
-    print('Erro:', resposta.json())
+    print('Erro:', resposta_imc.json())
+
+if resposta_tmb.status_code == 200:
+    print('TMB calculado:', resposta_tmb.json()['tmb'])
+else:
+    print('Erro:', resposta_tmb.json())
+
+
