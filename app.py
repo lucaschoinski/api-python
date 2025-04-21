8ac78ca719fb8cada518ab47f0b14f4304732f2f
Lucas Choinski <lucas.choinski3030@gmail.com>
2025-04-21 02:14:04 -0300

    feat: criando rota para consulta de tmb

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ def calc_imc(peso: float, altura: float):
     imc = peso / (altura * altura)
     return round(imc, 2)
 
-def calc_tmb(perfil: str, peso: float, altura: float, idade: float):
+def calc_tmb(perfil: str, peso: float, altura: float, idade: int):
     if perfil == "H":
         tmb = 88.362 + (13.397 * peso) + (4.799 * altura) - (5.677 * idade)
     if perfil == "M":
@@ -43,6 +43,23 @@ def calcular_imc():
     try:
         imc = calc_imc(float(peso), float(altura))
         return jsonify({'imc': imc})
+    except Exception as e:
+        return jsonify({'erro': str(e)}), 400
+
+@app.route('/tmb', methods=['POST'])
+def calcular_tmb():
+    data = request.json
+    peso = data.get('peso')
+    altura = data.get('altura')
+    idade = data.get('idade')
+    perfil = data.get('perfil')
+    
+    if not peso or not altura or not idade or not perfil:
+        return jsonify({'erro': 'Peso, altura, idade e perfil (H ou M) são obrigatórios.'}), 400
+    
+    try:
+        tmb = calc_tmb(str(perfil), float(peso), float(altura), int(idade))
+        return jsonify({'tmb': tmb})
     except Exception as e:
         return jsonify({'erro': str(e)}), 400
 
