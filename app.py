da4a82da4709804885a51dcf7e353749f880ff56
Lucas Choinski <lucas.choinski3030@gmail.com>
2025-04-21 02:10:37 -0300

    fix: Ajustando funcao, faltava para perfil feminino

--- app.py
+++ app.py
@@ -11,7 +11,10 @@ def calc_imc(peso: float, altura: float):
 def calc_tmb(perfil: str, peso: float, altura: float, idade: float):
     if perfil == "H":
         tmb = 88.362 + (13.397 * peso) + (4.799 * altura) - (5.677 * idade)
-        return tmb
+    if perfil == "M":
+        tmb = 447.593 + (9.247 * peso) + (3.098 * altura) - (4.330 * idade)
+
+    return tmb
 
 @app.route('/')
 def home():
